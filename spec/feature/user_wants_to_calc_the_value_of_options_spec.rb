61298c7b34b81f33db487603b3bf39f0213449d6
kennpat <kennedypr@gmail.com>
2015-03-11 20:59:32 -0400

    Added foundation and several other gems

---
+++ spec/feature/user_wants_to_calc_the_value_of_options_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'A User Wants To Calculate The Value Of An Option', %{
+  As a User
+  I would like to calculate the value of my option
+  So that I can use that value to determine proper treatment
+} do
+
+end
