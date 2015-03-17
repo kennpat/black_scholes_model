31e2d322e27753af493a12aeca9cb94056ff4aef
kennpat <kennedypr@gmail.com>
2015-03-16 22:51:19 -0400

    bringing up to date

--- spec/feature/user_wants_to_calc_the_value_of_options_spec.rb
+++ spec/feature/user_wants_to_calc_the_value_of_options_spec.rb
@@ -5,5 +5,21 @@ feature 'A User Wants To Calculate The Value Of An Option', %{
   I would like to calculate the value of my option
   So that I can use that value to determine proper treatment
 } do
+  scenario 'User correctly enters necessary information to calc the value of an option' do
+    visit root_path
+    expect(page).to have_content "Black-Scholes Model Calculator"
+    fill_in 'Fair Value', with: 23
+    fill_in 'Exercise Price', with: 40
+    fill_in 'Risk-Free Rate', with: 0.0019
+    fill_in 'Time to Maturity', with: 5
+    fill_in 'Volatility', with: 0.20
+    click_on 'Calculate'
 
+    expect(page).to have_content 'Your Calculated per option value:'
+    # add in calculated value as an expectation
+  end
+
+  # scenario 'User misses one of the necessary items when trying to calculate the value of an option' do
+  #
+  # end
 end
