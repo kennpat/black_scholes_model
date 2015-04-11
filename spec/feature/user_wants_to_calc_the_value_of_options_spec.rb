bbad2d6a80e712ef35f6182d5504d3196d648782
kennpat <kennedypr@gmail.com>
2015-04-11 18:27:05 -0400

    Form elements functional

--- spec/feature/user_wants_to_calc_the_value_of_options_spec.rb
+++ spec/feature/user_wants_to_calc_the_value_of_options_spec.rb
@@ -8,10 +8,10 @@ feature 'A User Wants To Calculate The Value Of An Option', %{
   scenario 'User correctly enters necessary information to calc the value of an option' do
     visit root_path
     expect(page).to have_content "Black-Scholes Model Calculator"
-    fill_in 'Fair Value', with: 23
-    fill_in 'Exercise Price', with: 40
-    fill_in 'Risk-Free Rate', with: 0.0019
-    fill_in 'Time to Maturity', with: 5
+    fill_in 'FairValue', with: 23
+    fill_in 'ExercisePrice', with: 40
+    fill_in 'RiskFreeRate', with: 0.0019
+    fill_in 'TimeToMaturity', with: 5
     fill_in 'Volatility', with: 0.20
     click_on 'Calculate'
 
