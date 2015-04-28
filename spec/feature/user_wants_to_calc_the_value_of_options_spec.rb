6537c6df768e784f1cb3fad562eda8be208e5943
kennpat <kennedypr@gmail.com>
2015-04-27 21:30:07 -0400

    Built in the javascript formula for the model

--- spec/feature/user_wants_to_calc_the_value_of_options_spec.rb
+++ spec/feature/user_wants_to_calc_the_value_of_options_spec.rb
@@ -8,11 +8,11 @@ feature 'A User Wants To Calculate The Value Of An Option', %{
   scenario 'User correctly enters necessary information to calc the value of an option' do
     visit root_path
     expect(page).to have_content "Black-Scholes Model Calculator"
-    fill_in 'FairValue', with: 23
-    fill_in 'ExercisePrice', with: 40
-    fill_in 'RiskFreeRate', with: 0.0019
-    fill_in 'TimeToMaturity', with: 5
-    fill_in 'Volatility', with: 0.20
+    fill_in 'S', with: 23
+    fill_in 'X', with: 40
+    fill_in 'r', with: 0.0019
+    fill_in 'T', with: 5
+    fill_in 'v', with: 0.20
     click_on 'Calculate'
 
     expect(page).to have_content 'Your Calculated per option value:'
